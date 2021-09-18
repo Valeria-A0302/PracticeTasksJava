0c742ab8b9533a7cd4e2a942bfcafb68e2aa657f
Atapina <valeria.atapina@mail.ru>
2021-09-18 22:30:21 +0300

    Задание 1.1

--- src/main/java/ru/ssau/tk/valeria/practice/Testing.java
+++ src/main/java/ru/ssau/tk/valeria/practice/Testing.java
@@ -1,4 +1,39 @@
 package ru.ssau.tk.valeria.practice;
 
 public class Testing {
+    public static void main(String[] args) {
+        Person objectOne = new Person();
+
+        objectOne.setFirstName("Ildar");
+        objectOne.getFirstName();
+
+        objectOne.setLastName("Idiyatov");
+        objectOne.getLastName();
+
+        objectOne.setPassportId(5764);
+        objectOne.getPassportId();
+
+        Person objectTwo = new Person();
+
+        objectTwo.setFirstName("Valeria");
+        objectTwo.getFirstName();
+
+        objectTwo.setLastName("Atapina");
+        objectTwo.getLastName();
+
+        objectTwo.setPassportId(2352);
+        objectTwo.getPassportId();
+
+        Person objectThree = new Person();
+
+        objectThree.setFirstName("Ivan");
+        objectThree.getFirstName();
+
+        objectThree.setLastName("Ivanov");
+        objectThree.getLastName();
+
+        objectThree.setPassportId(3423);
+        objectThree.getPassportId();
+
+    }
 }
