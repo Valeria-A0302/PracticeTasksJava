1f4afede1ff21ace3b5aa93bc7c5357da1154b10
Atapina <valeria.atapina@mail.ru>
2021-09-19 21:58:01 +0300

    Задание 1.4

---
+++ src/main/java/ru/ssau/tk/valeria/practice/Points.java
@@ -0,0 +1,18 @@
+package ru.ssau.tk.valeria.practice;
+
+public class Points {
+
+    public static double sum(double pointOne, double pointTwo){
+        return pointOne + pointTwo;
+    }
+    public static double subtract(double pointOne, double pointTwo){
+        return pointOne - pointTwo;
+    }
+    public static double multiply(double pointOne, double pointTwo){
+        return pointOne * pointTwo;
+    }
+    public static double divide(double pointOne, double pointTwo){
+        return pointOne / pointTwo;
+    }
+    private Points (){}
+}
--- src/main/java/ru/ssau/tk/valeria/practice/Testing.java
+++ src/main/java/ru/ssau/tk/valeria/practice/Testing.java
@@ -1,4 +1,10 @@
 package ru.ssau.tk.valeria.practice;
 
 public class Testing {
+    public static void main(String[] args) {
+        System.out.println("Сложение: "+Points.sum (23.5,1.5));
+        System.out.println("Вычитание: "+Points.subtract(23.6,2.6));
+        System.out.println("Умножение: "+Points.multiply(12.0,4.0));
+        System.out.println("Деление: "+Points.divide(49.0, 7.0));
+    }
 }
