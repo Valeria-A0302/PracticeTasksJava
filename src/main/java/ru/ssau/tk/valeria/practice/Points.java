889155b4738850a0a7ccba7f93c999cf95dbbe4d
Atapina <valeria.atapina@mail.ru>
2021-09-21 17:24:53 +0300

    Задание 1.5

--- src/main/java/ru/ssau/tk/valeria/practice/Points.java
+++ src/main/java/ru/ssau/tk/valeria/practice/Points.java
@@ -1,7 +1,27 @@
 package ru.ssau.tk.valeria.practice;
 
 public class Points {
+    public double x;
+    public double y;
+    public double z;
 
+    public Points (double x, double y, double z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    void setX (double x){this.x = x;}
+    double getX (){return x;}
+
+    void setY (double y){this.y = y;}
+    double getY (){return y;}
+
+    void setZ (double z){this.z = z;}
+    double getZ (){return z;}
+
+    public static Points enlarge(Points point, double someNumber){
+        return new Points(point.getX()*someNumber, point.getY()*someNumber, point.getZ()*someNumber);
+    }
     public static double sum(double pointOne, double pointTwo){
         return pointOne + pointTwo;
     }
@@ -14,5 +34,6 @@ public class Points {
     public static double divide(double pointOne, double pointTwo){
         return pointOne / pointTwo;
     }
-    private Points (){}
+
+
 }
--- src/main/java/ru/ssau/tk/valeria/practice/Testing.java
+++ src/main/java/ru/ssau/tk/valeria/practice/Testing.java
@@ -6,5 +6,10 @@ public class Testing {
         System.out.println("Вычитание: "+Points.subtract(23.6,2.6));
         System.out.println("Умножение: "+Points.multiply(12.0,4.0));
         System.out.println("Деление: "+Points.divide(49.0, 7.0));
+
+        Points point = new Points(3, 5, 7);
+        System.out.println(Points.enlarge(point,3).getX());
+        System.out.println(Points.enlarge(point,2).getY());
+        System.out.println(Points.enlarge(point,7).getZ());
     }
 }
