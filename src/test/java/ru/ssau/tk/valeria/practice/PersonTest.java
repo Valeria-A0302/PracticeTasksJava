8e4513b8772eb3ee4fc294c8ce66bd4d02ee181c
Atapina <valeria.atapina@mail.ru>
2021-09-30 20:18:28 +0300

    Задание 1.11

--- src/test/java/ru/ssau/tk/valeria/practice/PersonTest.java
+++ src/test/java/ru/ssau/tk/valeria/practice/PersonTest.java
@@ -9,16 +9,52 @@ import org.testng.annotations.*;
 
 public class PersonTest {
     @Test
-    public static void testingSettersAndGetters() {
+    public static void testingFirstName() {
         Person person = new Person();
         person.setFirstName("Valeria");
-        person.setLastName("Atapina");
-        person.setPassportId(1234);
+        Assert.assertEquals(person.getFirstName(), "Valeria");
+    }
 
+    @Test
+    public static void testingLastName() {
+        Person person = new Person();
+        person.setLastName("Atapina");
+        Assert.assertEquals(person.getLastName(), "Atapina");
+    }
+
+    @Test
+    public static void testingPassportId() {
+        Person person = new Person();
+        person.setPassportId(1234);
+        Assert.assertEquals(person.getPassportId(), 1234);
+    }
+
+    @Test
+    public static void testingConstructorWithoutParametersConstructor() {
+        Person person = new Person();
+        Assert.assertEquals(person.getFirstName(), "Unknown");
+        Assert.assertEquals(person.getLastName(), "Unknown");
+        Assert.assertEquals(person.getPassportId(), 0);
+    }
+
+    @Test
+    public static void testingConstructorWithOneParameter() {
+        Person person = new Person(1234);
+        Assert.assertEquals(person.getPassportId(), 1234);
+    }
+
+    @Test
+    public static void testingConstructorWithTwoParameter() {
+        Person person = new Person("Valeria", "Atapina");
+        Assert.assertEquals(person.getFirstName(), "Valeria");
+        Assert.assertEquals(person.getLastName(), "Atapina");
+    }
+
+    @Test
+    public static void testingConstructorWithThreeParameter() {
+        Person person = new Person("Valeria", "Atapina", 1234);
         Assert.assertEquals(person.getFirstName(), "Valeria");
         Assert.assertEquals(person.getLastName(), "Atapina");
         Assert.assertEquals(person.getPassportId(), 1234);
     }
-
-
 }
