41d6d61ed14439d2a2ff2858a2615ff9ba47972c
Atapina <valeria.atapina@mail.ru>
2021-10-16 21:17:19 +0300

    Задание 2.3

--- src/main/java/ru/ssau/tk/valeria/practice/MyArrays.java
+++ src/main/java/ru/ssau/tk/valeria/practice/MyArrays.java
@@ -1,15 +1,24 @@
 package ru.ssau.tk.valeria.practice;
 
 public class MyArrays {
-    public static int[] size (int length){
+    public static int[] size(int length) {
         return new int[length];
     }
-    public static int[] generator (int length){
-        int [] array = new int[length];
+
+    public static int[] generator(int length) {
+        int[] array = new int[length];
         array[0] = 2;
-        array[length-1] = 2;
-        for (int i = 1; i < length-1; i++){
-            array[i]=1;
+        array[length - 1] = 2;
+        for (int i = 1; i < length - 1; i++) {
+            array[i] = 1;
+        }
+        return array;
+    }
+
+    public static int[] oddArray(int length) {
+        int[] array = new int[length];
+        for (int element = 0; element < length; element++) {
+            array[element] = 2 * element + 1;
         }
         return array;
     }
--- src/test/java/ru/ssau/tk/valeria/practice/MyArraysTest.java
+++ src/test/java/ru/ssau/tk/valeria/practice/MyArraysTest.java
@@ -7,17 +7,28 @@ import static ru.ssau.tk.valeria.practice.MyArrays.*;
 
 public class MyArraysTest {
     @Test
-    public static void testSize(){
+    public static void testSize() {
         Assert.assertEquals(size(13).length, 13);
         Assert.assertEquals(size(1).length, 1);
         Assert.assertEquals(size(0).length, 0);
     }
+
     @Test
-    public static void testGenerator(){
-        Assert.assertEquals(generator(4)[0],2 );
-        Assert.assertEquals(generator(4)[1],1 );
-        Assert.assertEquals(generator(4)[2],1 );
-        Assert.assertEquals(generator(4)[3],2 );
+    public static void testGenerator() {
+        Assert.assertEquals(generator(4)[0], 2);
+        Assert.assertEquals(generator(4)[1], 1);
+        Assert.assertEquals(generator(4)[2], 1);
+        Assert.assertEquals(generator(4)[3], 2);
     }
 
+    @Test
+    public static void testOddArray() {
+        Assert.assertEquals(oddArray(7)[0], 1);
+        Assert.assertEquals(oddArray(7)[1], 3);
+        Assert.assertEquals(oddArray(7)[2], 5);
+        Assert.assertEquals(oddArray(7)[3], 7);
+        Assert.assertEquals(oddArray(7)[4], 9);
+        Assert.assertEquals(oddArray(7)[5], 11);
+        Assert.assertEquals(oddArray(7)[6], 13);
+    }
 }
