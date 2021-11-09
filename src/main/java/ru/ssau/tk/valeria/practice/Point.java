8aa3c57b3e12bfdce563465e7cdaf119e02d8106
Atapina <valeria.atapina@mail.ru>
2021-11-09 23:06:19 +0300

    Задание 3.15

--- src/main/java/ru/ssau/tk/valeria/practice/Point.java
+++ src/main/java/ru/ssau/tk/valeria/practice/Point.java
@@ -26,4 +26,9 @@ public class Point {
     public double getZ() {
         return z;
     }
+
+    @Override
+    public String toString() {
+        return "[" + x + ", " + y + ", " + z + "]";
+    }
 }
