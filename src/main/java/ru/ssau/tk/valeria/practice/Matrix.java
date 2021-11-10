6a823f4359af61483930f4104553e9662f986ab1
Atapina <valeria.atapina@mail.ru>
2021-11-11 00:24:15 +0300

    Задание 3.17

--- src/main/java/ru/ssau/tk/valeria/practice/Matrix.java
+++ src/main/java/ru/ssau/tk/valeria/practice/Matrix.java
@@ -26,4 +26,16 @@ public class Matrix {
     public int getLines() {
         return lines;
     }
+
+    @Override
+    public String toString() {
+        StringBuilder string = new StringBuilder();
+        for (int i = 0; i < columns; i++) {
+            for (int j = 0; j < lines; j++) {
+                string.append(getAt(i, j)).append(",");
+            }
+            string.append(";\n");
+        }
+        return string.toString();
+    }
 }
---
+++ src/test/java/ru/ssau/tk/valeria/practice/MatrixTest.java
@@ -0,0 +1,17 @@
+package ru.ssau.tk.valeria.practice;
+
+import org.testng.annotations.Test;
+
+public class MatrixTest {
+    @Test
+    public static void testToString() {
+        Matrix matrix = new Matrix(3, 2);
+        matrix.setAt(0, 0, 1);
+        matrix.setAt(0, 1, 2);
+        matrix.setAt(1, 0, 3);
+        matrix.setAt(1, 1, 4);
+        matrix.setAt(2, 0, 5);
+        matrix.setAt(2, 1, 6);
+        System.out.println(matrix);
+    }
+}
